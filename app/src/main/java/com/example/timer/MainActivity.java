5ef6d33ee886dc94a0e7c9c21aafe94946d90666
barte28 <bartek.teuerle@gmail.com>
2024-10-30 12:04:25 +0100

    handler

--- app/src/main/java/com/example/timer/MainActivity.java
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.timer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textViewCzas;
+    TextView textViewZapisanyCzas;
+    Button buttonZapisz;
+    Button buttonStart;
+    Button buttonPauza;
+    Button buttonReset;
+    int sekundy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewCzas = findViewById(R.id.textViewCzas);
+        textViewZapisanyCzas = findViewById(R.id.textViewZapisanyCzas);
+        buttonPauza = findViewById(R.id.buttonPauza);
+        buttonReset = findViewById(R.id.buttonReset);
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonZapisz = findViewById(R.id.buttonZapisz);
+        idzieCzas();
     }
+
+    private void idzieCzas(){
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                sekundy++;
+                s60 = sekundy%60;
+                textViewCzas.setText(String.valueOf(sekundy));
+                handler.postDelayed(this,1000);
+            }
+        });
+    }
+
 }
